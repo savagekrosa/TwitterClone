b001c2c533d64923f8edb3e617960c8006693524
GmixU <mjuszczak01@gmail.com>
2015-05-21 00:49:57 +0200

    Added simple authorization

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,11 +1,16 @@
+drop table hashtags;
+drop table comments;
+drop table posts;
+drop table users;
+drop table groups;
 create table Groups (
   GroupID varchar(30) primary key
 );
 create table Users (
   UserID serial primary key,
-  Mail varchar(50) unique not null,
-  Username varchar(20) unique not null,
-  Password varchar(30) not null,
+  Mail varchar(80) unique not null,
+  Username varchar(50) unique not null,
+  Password varchar(255) not null,
   UserGroup varchar(30) not null references Groups
 );
 create table Posts (
@@ -32,3 +37,9 @@ create index CommentAuthorIndex on Comments(author asc);
 create index PostAuthorIndex on Posts(author asc);
 create index CommentPostFKeyIndex on Comments(postid asc);
 insert into groups(groupid) values('admin'), ('moderator'), ('blogger');
+--login: admin pass: admin
+insert into users(mail, username, password, usergroup) values ('admin', 'admin', 'jGl25bVBBBW96Qi9Te4V37Fnqchz/Eu4qB9vKrRIqRg=', 'admin');
+--login: blogger pass: blogger
+insert into users(mail, username, password, usergroup) values ('blogger', 'blogger', 'B1XdNqAjjlzY+Kos7g3gKPkOpz7lVKGFJ4aN26lSIUg=', 'blogger');
+--login: moderator pass: moderator
+insert into users(mail, username, password, usergroup) values ('moderator', 'moderator', 'z94spRiK+3vdBpHHvviHurp4twmq3ejoxTUynVdR5v4=', 'moderator');
